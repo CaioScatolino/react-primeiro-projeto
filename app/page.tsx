b819428acd343e9dadbf5671564bdfb2654b00bb
Caio <caio.scatolino@outlook.com>
2026-08-13 21:05:30 -0300

    feat: add Square and Circle components to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Square, Circle } from "../components/Geo";
+
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-xl text-red-500">Hello World</h1>
+
+      <Square />
+      <Circle />
+
     </div>
   )
 }
---
+++ components/Geo.tsx
@@ -0,0 +1,15 @@
+export const Square = () => {
+    return (
+        <div>
+            <button className="bg-orange-700 w-52 h-52 text-white"> Texto</button>
+        </div>
+    )
+}
+export const Circle = () => {
+    return (
+        <div>
+            <button className="bg-orange-700 w-52 h-52 text-white rounded-full"> Texto</button>
+        </div>
+    )
+}
+
